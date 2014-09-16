a5bc5d59dcf94e81f1c8246079a300f2d40d9cb0
sebastian ojeda <sebastianojeda>
2014-09-15 18:07:48 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+//All of this code goes to my hamburger.//
+(function(){
+    var $mainWrapper = $("#main-wrapper");
+
+    $('.menu-icon').click(function(){
+        $(this).toggleClass('active');
+        $mainWrapper.toggleClass('active');
+    });
+
+
+})();
+
+
+$(".nav-btn").click(function(){
+      $("#sidebar").toggleClass("active");
+      $("#content").toggleClass("active");
+      $(".nav-btn").toggleClass("active");
+});
